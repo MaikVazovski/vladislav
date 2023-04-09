9d5c374a915faddfed7dc6f277c131125196e5e9
MaikVazovski <mvazo.vski461@gmail.com>
2023-04-10 00:02:00 +0500

    commit

--- суммацифрстепени.py
+++ суммацифрстепени.py
@@ -1,8 +1,8 @@
 import numpy as np
 a = int(input('Введите основание: '))
 b = int(input('Введите степень: '))
-c = a**b
-print(f'Число = {c}')
-x = [int(d) for d in str(c)]
+result = a**b
+print(f'Число = {result}')
+x = [int(d) for d in str(result)]
 sum = np.sum(x)
 print(f'Сумма цифр степени = {sum}')
