1e0be391a2b7dbcffb3a3150aebca4a3f4277962
MaikVazovski <mvazo.vski461@gmail.com>
2023-04-10 08:29:34 +0500

    commit

--- кратные.py
+++ кратные.py
@@ -1,11 +1,11 @@
-a = int(input('Введи число, которое будем проверять на делимость: '))
-b = int(input('Введите первый делитель: '))
-c = int(input('Введите второе делитель: '))
-if a % b == 0 and a % c == 0:
-    print(f'Число кратно {b} и {c}')
-elif a % b == 0:
-    print(f'Число кратно только {b}')
-elif a % c == 0:
-    print(f'Число кратно только {c}')
+one_value = int(input('Введи число, которое будем проверять на делимость: '))
+two_value = int(input('Введите первый делитель: '))
+three_value = int(input('Введите второе делитель: '))
+if one_value % two_value == 0 and one_value % three_value == 0:
+    print(f'Число кратно {two_value} и {three_value}')
+elif one_value % two_value == 0:
+    print(f'Число кратно только {two_value}')
+elif one_value % three_value == 0:
+    print(f'Число кратно только {three_value}')
 else:
-    print(f'Число не кратно {b} и {c}')
+    print(f'Число не кратно {two_value} и {three_value}')
