491250229529f38d8494739e58c668f2035e8d72
MaikVazovski <mvazo.vski461@gmail.com>
2023-04-11 19:43:39 +0500

    commit

---
+++ пайгейм.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption('Программа')
+running = True
+while running:
+    
+    
+    
+    pygame.display.update()
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    
+    
